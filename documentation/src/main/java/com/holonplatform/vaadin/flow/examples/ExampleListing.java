96dbea528b42396be88cbcb1dcd99a3ee51a7a7a
Riccardo Righi <rr9379@gmail.com>
2019-01-09 11:34:15 +0100

    Documentation (wip)

--- documentation/src/main/java/com/holonplatform/vaadin/flow/examples/ExampleListing.java
+++ documentation/src/main/java/com/holonplatform/vaadin/flow/examples/ExampleListing.java
@@ -16,14 +16,19 @@
 package com.holonplatform.vaadin.flow.examples;
 
 import com.holonplatform.core.datastore.DataTarget;
+import com.holonplatform.core.datastore.Datastore;
 import com.holonplatform.core.property.NumericProperty;
+import com.holonplatform.core.property.PropertyBox;
 import com.holonplatform.core.property.PropertySet;
 import com.holonplatform.core.property.StringProperty;
 import com.holonplatform.vaadin.flow.components.BeanListing;
 import com.holonplatform.vaadin.flow.components.Components;
 import com.holonplatform.vaadin.flow.components.PropertyListing;
+import com.holonplatform.vaadin.flow.data.DatastoreDataProvider;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.data.provider.DataProvider;
 
+@SuppressWarnings("unused")
 public class ExampleListing {
 
 	// tag::propertydef[]
@@ -32,7 +37,9 @@ public class ExampleListing {
 	static final PropertySet<?> SUBJECT = PropertySet.of(ID, NAME);
 	// end::propertydef[]
 
-	private static final DataTarget<?> TARGET = DataTarget.named("subjects");
+	// tag::target[]
+	static final DataTarget<?> TARGET = DataTarget.named("subjects");
+	// end::target[]
 
 	public void listing1() {
 		// tag::listing1[]
@@ -49,6 +56,16 @@ public class ExampleListing {
 
 		private Long id;
 		private String name;
+
+		public MyBean() {
+			super();
+		}
+
+		public MyBean(Long id, String name) {
+			super();
+			this.id = id;
+			this.name = name;
+		}
 
 		public Long getId() {
 			return id;
@@ -77,6 +94,102 @@ public class ExampleListing {
 
 		new VerticalLayout().add(listing.getComponent()); // <3>
 		// end::listing2[]
+	}
+
+	public void listing3() {
+		// tag::listing3[]
+		PropertyListing listing = PropertyListing.builder(SUBJECT) // <1>
+				.items(PropertyBox.builder(SUBJECT).set(ID, 1L).set(NAME, "One").build()).build();
+
+		listing = PropertyListing.builder(SUBJECT) // <2>
+				.addItem(PropertyBox.builder(SUBJECT).set(ID, 1L).set(NAME, "One").build())
+				.addItem(PropertyBox.builder(SUBJECT).set(ID, 2L).set(NAME, "Two").build()).build();
+		// end::listing3[]
+	}
+
+	public void listing4() {
+		// tag::listing4[]
+		BeanListing<MyBean> listing = BeanListing.builder(MyBean.class) // <1>
+				.items(new MyBean(1L, "One"), new MyBean(2L, "Two")).build();
+
+		listing = BeanListing.builder(MyBean.class) // <2>
+				.addItem(new MyBean(1L, "One")) //
+				.addItem(new MyBean(2L, "Two")).build();
+		// end::listing4[]
+	}
+
+	public void listing5() {
+		// tag::listing5[]
+		DataProvider<PropertyBox, ?> dataProvider = getPropertyBoxDataProvider(); // <1>
+
+		PropertyListing listing = PropertyListing.builder(SUBJECT).dataSource(dataProvider) // <2>
+				.build();
+		// end::listing5[]
+	}
+
+	public void listing6() {
+		// tag::listing6[]
+		DataProvider<MyBean, ?> dataProvider = getBeanDataProvider(); // <1>
+
+		BeanListing<MyBean> listing = BeanListing.builder(MyBean.class).dataSource(dataProvider) // <2>
+				.build();
+		// end::listing6[]
+	}
+
+	public void listing7() {
+		PropertyBox itemToRefresh = null;
+		// tag::listing7[]
+		PropertyListing listing = PropertyListing.builder(SUBJECT).dataSource(getDataProvider()).build(); // <1>
+
+		listing.refresh(); // <2>
+		listing.refreshItem(itemToRefresh); // <3>
+		// end::listing7[]
+	}
+
+	public void listing8() {
+		// tag::listing8[]
+		Datastore datastore = getDatastore();
+
+		PropertyListing listing = PropertyListing.builder(SUBJECT) // <1>
+				.dataSource(DatastoreDataProvider.create(datastore, TARGET, SUBJECT)) // <2>
+				.build();
+		// end::listing8[]
+	}
+
+	public void listing9() {
+		// tag::listing9[]
+		Datastore datastore = getDatastore();
+
+		PropertyListing listing = PropertyListing.builder(SUBJECT) // <1>
+				.dataSource(datastore, TARGET) // <2>
+				.build();
+		// end::listing9[]
+	}
+
+	public void listing10() {
+		// tag::listing10[]
+		PropertyListing listing = PropertyListing.builder(SUBJECT) //
+				.dataSource(getDatastore(), TARGET) // <1>
+				.withQueryFilter(NAME.isNotNull()) // <2>
+				.withQuerySort(ID.asc()) // <3>
+				.build();
+		// end::listing10[]
+	}
+
+	private static DataProvider<PropertyBox, ?> getDataProvider() {
+		return null;
+	}
+
+	private static DataProvider<PropertyBox, ?> getPropertyBoxDataProvider() {
+		return null;
+	}
+
+	private static DataProvider<MyBean, ?> getBeanDataProvider() {
+		return null;
+	}
+
+	private static Datastore getDatastore() {
+		return null;
 	}
 
 }
